f69a5575f4ac4dc1d6f64bc8fdbfa7caa42a23c7
maxlov <maxwellloganlovell@gmail.com>
2021-07-21 12:42:29 -0400

    style_fix 0-lockboxes\ncode_add edgecase

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,22 +3,22 @@
 
 
 def canUnlockAll(boxes):
-	'''determines if all boxes could be opened'''
-	if (len(boxes) <= 1):
-		return True
-	keys = []
-	for key in boxes[0]:
-		if key not in keys:
-			keys.append(key)
-		boxes[0] = None
-	idx = 0
-	while (idx < len(keys)):
-		if (boxes[keys[idx]] != None):
-			for key in boxes[keys[idx]]:
-				if key not in keys:
-					keys.append(key)
-			boxes[keys[idx]] = None
-		idx += 1
-	if (all(box == None for box in boxes)):
-		return True
-	return False
+    '''determines if all boxes could be opened'''
+    if (len(boxes) <= 1):
+        return True
+    keys = []
+    for key in boxes[0]:
+        if key not in keys:
+            keys.append(key)
+        boxes[0] = None
+    idx = 0
+    while (idx < len(keys)):
+        if (boxes[keys[idx]] is not None):
+            for key in boxes[keys[idx]]:
+                if key not in keys:
+                    keys.append(key)
+            boxes[keys[idx]] is None
+        idx += 1
+    if (all(box is None for box in boxes)):
+        return True
+    return False
