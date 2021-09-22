122405c2a44d12e05e4342dfc57e431a1d40c6d2
maxlov <maxwellloganlovell@gmail.com>
2021-09-22 18:51:47 -0400

    [Fix] removed print statement and added line for pep8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,12 +3,12 @@
 Main file for testing
 """
 
+
 def validUTF8(data):
     '''validates data'''
     num_ones = 0
     for value in data:
         value = str(bin(value))[2:].rjust(8, '0')
-        print(value)
         if num_ones > 0:
             num_ones -= 1
             if value[:2] != "10":
