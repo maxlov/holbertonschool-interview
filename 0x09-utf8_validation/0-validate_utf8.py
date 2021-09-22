eaff225b4a642c403b3a22ba47cfc85c23f4c3e7
maxlov <maxwellloganlovell@gmail.com>
2021-09-22 18:49:50 -0400

    [Create] utf8 validation code

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+def validUTF8(data):
+    '''validates data'''
+    num_ones = 0
+    for value in data:
+        value = str(bin(value))[2:].rjust(8, '0')
+        print(value)
+        if num_ones > 0:
+            num_ones -= 1
+            if value[:2] != "10":
+                return False
+        elif value[0] == '1':
+            num_ones = len(value.split('0')[0]) - 1
+    return True
---
+++ 0x09-utf8_validation/REAMDE.md
@@ -0,0 +1,2 @@
+#0x09. UTF-8 Validation
+Validates UTF8 for list of numbers
