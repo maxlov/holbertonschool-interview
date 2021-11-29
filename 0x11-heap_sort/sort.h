d44a311b3e60a56e08106a6befb205ad8278491c
maxlov <maxwellloganlovell@gmail.com>
2021-11-28 23:04:15 -0500

    [Update] Added missing function declaration

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
+void heapify(int array[], int size, int i, int length);
 void heap_sort(int *array, size_t size);
 
 #endif /* SORT_H */
