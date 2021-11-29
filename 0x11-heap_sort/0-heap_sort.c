9e18d6253400d1274d5906a2bd0a73ed691c080a
maxlov <maxwellloganlovell@gmail.com>
2021-11-28 22:33:00 -0500

    [Create] README, sort script

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sort.h"
+
+/**
+ * heapify - turns a subtree into a heap at node i
+ *
+ * @array: array being sorted
+ * @size: size of subtree
+ * @i: node of root
+ * @length: length of array
+ */
+void heapify(int array[], int size, int i, int length)
+{
+	int largest = i;
+	int l = 2 * i + 1;
+	int r = 2 * i + 2;
+	int temp;
+
+	if (l < size && array[l] > array[largest])
+		largest = l;
+
+	if (r < size && array[r] > array[largest])
+		largest = r;
+
+	if (largest != i)
+	{
+		temp = array[largest];
+		array[largest] = array[i];
+		array[i] = temp;
+		print_array(array, length);
+
+		heapify(array, size, largest, length);
+	}
+}
+
+/**
+ * heap_sort - Sorts an array in ascending order using heap sort algorithm
+ *
+ * @array: array being sorted
+ * @size: number being tested
+ *
+ */
+void heap_sort(int *array, size_t size)
+{
+	int temp;
+
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
+
+	for (int i = size - 1; i > 0; i--)
+	{
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+
+		heapify(array, i, 0, size);
+	}
+}
---
+++ 0x11-heap_sort/README.md
@@ -0,0 +1,6 @@
+# #0x11. Heap sort
+## In this project, I write a function that sorts an array of integers in ascending order using the Heap sort algorithm
+
+* Prototype: void heap_sort(int *array, size_t size)
+* This is implemented in the sift-down heap sort algorithm
+* The array is printed after each time you swap two elements
