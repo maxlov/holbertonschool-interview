8dc1a5098990b2e5221e57472617bb65ad8c70a4
maxlov <maxwellloganlovell@gmail.com>
2022-01-05 12:28:19 -0500

    [Fix] styling issues, added documentation

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
+
 def rotate_2d_matrix(matrix):
+    ''' Rotates positions of matrix 90 degrees clockwise'''
     size = len(matrix)
-    map_matrix = [[[0, 0] for i in range(size)] for j in range(size)]
+    map = [[[0, 0] for i in range(size)] for j in range(size)]
     empty_matrix = [[0 for i in range(size)] for j in range(size)]
     rotPosY = size - 1
     for x in range(0, size):
         for y in range(0, size):
-            map_matrix[x][y][0] = y
-            map_matrix[x][y][1] = rotPosY
+            map[x][y][0] = y
+            map[x][y][1] = rotPosY
         rotPosY -= 1
     for x in range(0, size):
         for y in range(0, size):
-            empty_matrix[map_matrix[x][y][0]][map_matrix[x][y][1]] = matrix[x][y]
+            empty_matrix[map[x][y][0]][map[x][y][1]] = matrix[x][y]
     for x in range(0, size):
         for y in range(0, size):
             matrix[x][y] = empty_matrix[x][y]
