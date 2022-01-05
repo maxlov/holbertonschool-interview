28857306309532a02245f5e4d3045a5f73f553b6
maxlov <maxwellloganlovell@gmail.com>
2022-01-05 14:27:29 -0500

    [Fix] more formatting

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-
+""" Rotates posiitons of matrix """
 
 def rotate_2d_matrix(matrix):
-    ''' Rotates positions of matrix 90 degrees clockwise'''
+    """ Rotates positions of matrix 90 degrees clockwise """
     size = len(matrix)
-    map = [[[0, 0] for i in range(size)] for j in range(size)]
+    matrixMap = [[[0, 0] for i in range(size)] for j in range(size)]
     empty_matrix = [[0 for i in range(size)] for j in range(size)]
     rotPosY = size - 1
-    for x in range(0, size):
-        for y in range(0, size):
-            map[x][y][0] = y
-            map[x][y][1] = rotPosY
+    for x in range(size):
+        for y in range(size):
+            matrixMap[x][y][0] = y
+            matrixMap[x][y][1] = rotPosY
         rotPosY -= 1
-    for x in range(0, size):
-        for y in range(0, size):
-            empty_matrix[map[x][y][0]][map[x][y][1]] = matrix[x][y]
-    for x in range(0, size):
-        for y in range(0, size):
+    for x in range(size):
+        for y in range(size):
+            empty_matrix[matrixMap[x][y][0]][matrixMap[x][y][1]] = matrix[x][y]
+    for x in range(size):
+        for y in range(size):
             matrix[x][y] = empty_matrix[x][y]
